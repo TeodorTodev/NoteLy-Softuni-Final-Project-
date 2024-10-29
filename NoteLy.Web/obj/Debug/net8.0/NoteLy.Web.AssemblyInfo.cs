5a375ae095f91674296780d4c9b632e9ad798b65
TeodorTodev <tedi_todev@abv.bg>
2024-10-29 18:23:15 +0200

    fix css for add, edit and delete btns for every view

--- NoteLy.Web/obj/Debug/net8.0/NoteLy.Web.AssemblyInfo.cs
+++ NoteLy.Web/obj/Debug/net8.0/NoteLy.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteLy.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c5525910c29922bd512d12aefad02518a75fe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee41c4213150e3a357e5ab0e38120e013785ee5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteLy.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteLy.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
